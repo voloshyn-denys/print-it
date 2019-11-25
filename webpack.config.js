2edc019d1cfbc0e50dd8ec18c919bb252b674f36
Denys Voloshyn <denys.voloshyn@ria.com>
2019-11-25 12:05:33 +0200

    fix index template

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "index_bundle.js",
